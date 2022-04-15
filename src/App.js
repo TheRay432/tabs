c8b4525b06b28cb5b06162d45455c206c19acba5
ray <ll9907891@gmail.com>
2022-04-15 18:03:15 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,42 +1,93 @@
 import "./styles/all.css";
 import TrainIcon from "./icons/icon3.svg";
 import { useEffect, useRef, useState } from "react";
+import ProductBtn from "./components/ProductBtn";
 function App() {
-  /*   const tripArr = [
-    { TypeName: "鐵道旅遊", TypeCode: 1 },
-    { TypeName: "巴士旅遊", TypeCode: 2 },
-    { TypeName: "郵輪旅遊", TypeCode: 3 },
-  ]; */
-  /* const [tripTypeList, setTripTypeList] = useState(tripArr); */
   const [width1, setWidth1] = useState(null);
+  const [productArr, setProductArr] = useState(null);
+  const a = [
+    "金質獎",
+    "登山健行",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+    "建築走讀",
+  ];
+  const [trainActive, setTrainActive] = useState({
+    state1: false,
+    state2: false,
+    state3: false,
+  });
   const ref = useRef(null);
+  const testRef = useRef(null);
   useEffect(() => {
     const width2 = ref.current ? ref.current.offsetWidth : 0;
-    setWidth1(width2);
-    console.log(width2);
+    const width3 = testRef.current ? testRef.current.offsetWidth : 0;
+    setWidth1(width3);
   }, [width1]);
+  useEffect(() => {
+    setProductArr([...a]);
+  }, []);
+  const trainBtnClick = (state) => {
+    switch (state) {
+      case "state1":
+        setTrainActive({ ...trainActive, state1: !trainActive.state1 });
+        break;
+      case "state2":
+        setTrainActive({ ...trainActive, state2: !trainActive.state2 });
+        break;
+      case "state3":
+        setTrainActive({ ...trainActive, state3: !trainActive.state3 });
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="tab_container">
       <div className="tab_inner_container">
         <div className="transportDiv">
           <h3>遊玩交通</h3>
           <div className="tripBtnContainer" ref={ref}>
-            <button className="tripBtn">
-              <img src={TrainIcon} alt="" />
-              <p>鐵道旅遊</p>
+            <button
+              className={trainActive.state1 ? "tripBtn active" : "tripBtn"}
+              onClick={() => trainBtnClick("state1")}
+            >
+              <img className="activeIcon" src={TrainIcon} alt="" />
+              <p className="tt">鐵道旅遊</p>
             </button>
-            <button className="tripBtn">
-              <img src={TrainIcon} alt="" />
+            <button
+              className={trainActive.state2 ? "tripBtn active" : "tripBtn"}
+              onClick={() => trainBtnClick("state2")}
+            >
+              <img className="activeIcon" src={TrainIcon} alt="" />
               <p>巴士旅遊</p>
             </button>
-            <button className="tripBtn">
-              <img src={TrainIcon} alt="" />
+            <button
+              className={trainActive.state3 ? "tripBtn active" : "tripBtn"}
+              onClick={() => trainBtnClick("state3")}
+            >
+              <img className="activeIcon" src={TrainIcon} alt="" />
               <p>郵輪旅遊</p>
             </button>
           </div>
         </div>
         <div className="productDiv">
           <h3>產品規格</h3>
+          <div className="productBtnContainer" ref={testRef}>
+            {/*  {productArr && productArr.map((item) => <ProductBtn item={item} />)} */}
+          </div>
+          <div>
+            <p className="blue">顯示</p>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/ProductBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductBtn = ({ item }) => {
+  return <div className="testBtn">{item}</div>;
+};
+
+export default ProductBtn;
